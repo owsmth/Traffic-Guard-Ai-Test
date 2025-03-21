34c14fd9b89a6cf698e877a1dedb987da0b688bb
nixguin <ntony422@gmail.com>
2025-03-21 11:55:25 -0400

    Refactor App layout and Navbar styling; add layout.css for improved design

--- Federated-Home-System/federated-home-system/src/components/Navbar.jsx
+++ Federated-Home-System/federated-home-system/src/components/Navbar.jsx
@@ -1,29 +1,52 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">🏠 FrED IoT Home System</Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="white"
+      fixed="top"
+      className="py-2"
+    >
+      <Container fluid="lg">
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          🏠 FrED IoT Home System
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#iot-devices">IoT Devices</Nav.Link>
-            <Nav.Link href="#network-status">Network Status</Nav.Link>
-            <NavDropdown title="More Options" id="collapsible-nav-dropdown">
+            <Nav.Link href="#iot-devices" className="px-3">
+              IoT Devices
+            </Nav.Link>
+            <Nav.Link href="#network-status" className="px-3">
+              Network Status
+            </Nav.Link>
+            <NavDropdown
+              title="More Options"
+              id="collapsible-nav-dropdown"
+              className="custom-dropdown px-3"
+              align="end"
+            >
               <NavDropdown.Item href="#settings">Settings</NavDropdown.Item>
-              <NavDropdown.Item href="#activity-logs">Activity Logs</NavDropdown.Item>
+              <NavDropdown.Item href="#activity-logs">
+                Activity Logs
+              </NavDropdown.Item>
               <NavDropdown.Item href="#alerts">Alerts</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#support">Support</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#notifications">Notifications</Nav.Link>
-            <Nav.Link eventKey={2} href="#account">Exit ↪</Nav.Link>
+            <Nav.Link href="#notifications" className="px-3">
+              Notifications
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#account" className="px-3">
+              Exit ↪
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
